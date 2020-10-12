0ac85bb805277ed9c2f0a4ba6876eaace56f6a4d
himanshuteotia <himanshuteotia7@gmail.com>
2020-10-12 14:35:13 +0530

    index-test:add regex to check objectid and created date

--- src/__tests__/index-test.ts
+++ src/__tests__/index-test.ts
@@ -1,4 +1,6 @@
 import BlockObjBuilder from '../index';
+const ISO_DATE_STRING = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
+const OBJECT_ID_STRING = /^[a-f\d]{24}$/i;
 
 const buildObj = {
   blockId: '5f81dcd62c77cd23383566b6',
@@ -11,11 +13,12 @@ const buildObj = {
 };
 describe('Block builder obj', () => {
   it('BlockTextManager', async () => {
-    const blockObj = new BlockObjBuilder();
-    expect(blockObj.build()).toEqual({
-      ...buildObj,
-      blockId: expect.anything(),
-      created: expect.anything()
+    const blockBuilderObj = new BlockObjBuilder();
+    const blockObj = blockBuilderObj.build();
+    expect(buildObj).toEqual({
+      ...blockObj,
+      blockId: expect.stringMatching(OBJECT_ID_STRING),
+      created: expect.stringMatching(ISO_DATE_STRING)
     });
   });
 });
